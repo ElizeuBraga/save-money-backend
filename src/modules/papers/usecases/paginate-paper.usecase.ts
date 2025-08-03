84be8b319fb9cc670937183dad28a9ec93778fe7
Elizeu Braga Santos <elizeubragasantos@gmail.com>
2025-08-02 21:43:36 -0300

    tables

--- src/modules/papers/usecases/paginate-paper.usecase.ts
+++ src/modules/papers/usecases/paginate-paper.usecase.ts
@@ -4,6 +4,8 @@ import { AuthorizationService } from 'src/modules/authorization/services/authori
 import { RoleEnum } from 'src/modules/common/types/enum'
 import { Repository } from 'typeorm'
 import { Paper } from '../../common/entities/Paper.entity'
+import sumBy from '../../common/utils/sum-by.util'
+import percent from '../../common/utils/percent.util.'
 
 @Injectable()
 export class PaginatePaperUsecase {
@@ -18,8 +20,33 @@ export class PaginatePaperUsecase {
   async exec() {
     this.authorizationService.validate(this.roles)
 
-    return this.repository.find({
-      select: { id: true, name: true },
+    const papers = await this.repository.find({
+      select: {
+        id: true,
+        name: true,
+        investments: {
+          id: true,
+          price: true,
+          bank: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+      relations: ['investments.bank'],
     })
+
+    const investments = papers.flatMap((paper) => paper.investments)
+    const totalInvestments = sumBy(investments, 'price')
+    for (const paper of papers) {
+      paper.totalInvested = paper.totalInvested = sumBy(
+        paper.investments,
+        'price',
+      )
+
+      paper.percentInvested = percent(paper.totalInvested, totalInvestments)
+    }
+
+    return papers
   }
 }
