668664f54a4e2a3e1a6b03c90ba5a99b0a0006a8
Elizeu Braga Santos <elizeubragasantos@gmail.com>
2025-08-05 16:23:12 -0300

    login  page

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -14,6 +14,9 @@ import { AuthService } from '../services/auth.service'
 import { Public } from '../utils/public.metadata'
 import { CreateUserUsecase } from '../usecases/create-user.usecase'
 import { CreateUserDto } from '../dto/create-user-dto'
+import { ApiBody } from '@nestjs/swagger'
+import { LoginDto } from '../dto/login-dto'
+import { Login } from '../decorators/login.metadata'
 
 @Controller('auth')
 export class AuthController {
@@ -26,9 +29,10 @@ export class AuthController {
    * Rota limitada à 5 requisições (acertando ou errando a senha).
    * A rota é liberada para o usuário após 1 minuto
    */
-  @Public()
+  @ApiBody({ type: LoginDto })
+  @Login()
   @UseGuards(MultipleAuthGuard)
-  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  // @Throttle({ default: { limit: 5, ttl: 60000 } })
   @Post('login')
   @HttpCode(200)
   login(@Request() { user }: { user: User }) {
